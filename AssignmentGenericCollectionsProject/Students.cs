68820d2feaa4e8e4ff7678ae7b370f7e9b1f07dc
lindmark421@gmail.com <lindmark421@gmail.com>
2019-08-05 09:01:42 -0400

    Add project files.

---
+++ AssignmentGenericCollectionsProject/Students.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentGenericCollectionsProject {
+    // Student Class
+    public class Student {
+        public string Name { get; set; }
+        public decimal GPA { get; set; }
+    }
+
+}
